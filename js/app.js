93e1a011183549403a7e728bc5148c0d5f4748b6
Samuel Cochrane <SamuelDavisCochrane@gmail.com>
2015-11-15 21:43:57 -0800

    Fixes #3, Fixes #4
    added form, added some stub javascript for the controller

---
+++ js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+	
+angular.module('ourApp', [])
+
+.controller('ourCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.submitForm = function(ourForm) {
+		alert("Thanks for the interest, "+ourForm.firstName.$modelValue+"!");
+		console.log(ourForm);
+	}
+	
+	$scope.resetForm = function() {
+		alert("Ok, let's try again");
+		document.getElementById("form-element").reset();
+	}
+	
+	$scope.getAge = function(DOB) {
+	
+		//placeholder
+		return true;
+	}
+	
+	$scope.passwordMatch = function() {
+	
+		//placeholder
+		return true;
+	}
+	
+}]);
